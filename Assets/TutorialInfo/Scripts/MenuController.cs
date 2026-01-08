a408b4d094e3d9f48e5fe2789a8994ef1a73be45
Sabrina Sanchez <sa.sanchez.alum@fp.mercedarias.es>
2026-01-08 18:03:06 +0100

    BotonPlay Menu de inicio Capas de enemigos

---
+++ Assets/TutorialInfo/Scripts/MenuController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; //es la librería que incluimos para poder usar otros elementos
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("AventurerosEsqueletos");
+    }
+}
